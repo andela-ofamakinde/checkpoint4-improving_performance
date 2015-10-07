7959324047dc7360257d895ca65bf69b1077f4e2
andela-ofamamkinde <olatoyosi.famakinde@andela.co>
2015-10-07 10:01:38 +0100

    Expire Author cache

--- app/helpers/author_helper.rb
+++ app/helpers/author_helper.rb
@@ -2,6 +2,6 @@ module AuthorHelper
   def cache_key_for_authors
     count          = Author.count
     max_updated_at = Author.maximum(:updated_at).try(:utc).try(:to_s, :number)
-    "authors/all-#{count}-#{max_updated_at}"
+    "cache-key#{count}-#{max_updated_at}"
   end
 end
